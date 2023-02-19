2f44b713ae668a9ac8719c2281d3f28887045ae4
Spencer Boggs <spencerdboggs@gmail.com>
2023-02-19 15:26:28 -0800

    Create array-generator.py

---
+++ array-generator.py
@@ -0,0 +1,12 @@
+import math
+while True:
+    result = float(input("Starting value: "))
+    step = float(input("Step size: "))
+    count = int(input("Steps: "))
+    decimal = int(input("Decimal places: "))
+    array = []
+    for x in range(count):
+        result = step + result
+        result = round(result, decimal)
+        array.append(result)
+    print(array)
